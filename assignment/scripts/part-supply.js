62bbde9649d81e29435840bdbe295405d57e084c
Tina Tram <tinatram07@gmail.com>
2022-10-14 20:18:11 -0500

    added for loop to add all numbers in supplyChanges array, rewrote for loop as for of loop, wrote while loop to add totalParts to boxes

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -45,10 +45,27 @@ for (let i = 0; i < supplyChanges.length; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+console.log(supplyChanges);
+for (let supply of supplyChanges) {
+    if (supply > 0) {
+        console.log('Added', supply, 'parts.');
+    }
+    else if (supply == 0) {
+        console.log('No Change.');
+    }
+    else if (supply < 0) {
+        console.log('Removed', Math.abs(supply), 'parts.');
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalParts = 0;
+for(let i = 0; i < supplyChanges.length; i++) {
+    totalParts += supplyChanges[i];
+}
+console.log(totalParts)
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -57,3 +74,11 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let num_boxes = 0;
+totalParts = 572;
+while (totalParts > 6) {
+    totalParts -= 7;
+    num_boxes ++;
+}
+console.log('Boxes filled:', num_boxes);
+console.log('Number of parts left over:', totalParts);
